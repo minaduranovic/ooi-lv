67cd539ff9d2b139ad4c89dc34677a40f19d4a7c
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-11-01 23:16:14 +0100

    zadaca 2 gotova

--- zadace/zadaca2.jl
+++ zadace/zadaca2.jl
@@ -5,7 +5,7 @@ m = Model(GLPK.Optimizer)
 @variable(m, x1>=0)
 @variable(m, x2>=0)
 
-@objective(m, Max, 4x1-3x2)
+@objective(m, Min, 4x1-3x2)
 
 @constraint(m, constraint1, 8x1+8x2<=7)
 @constraint(m, constraint2, 7x1+6x2<=9)
