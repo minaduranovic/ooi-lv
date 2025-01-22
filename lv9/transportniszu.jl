c7ab4b98feaeef67efb61c704f35138d066f922b
Mina Duranovic <mduranovic2@etf.unsa.ba>
2025-01-22 15:28:37 +0100

    lv9

--- lv9/transportniszu.jl
+++ lv9/transportniszu.jl
@@ -1,75 +1,93 @@
 #Zadatke radile Mina Duranović 19290 i Sara Dautbegović 19463
 
-# autori zadatka nisu navedeni
-
-function nadji_pocetno_SZU(C, I, O)
-    m = length(I)
+function nadji_pocetno_SZU(C,I,O)
+    i = sum(I)
+    m = size(I, 1)
+    o = sum(O)
     n = length(O)
-    sumaI = sum(I)
-    sumaO = sum(O)
-
-    # provjera balansiranosti
-    if sumaI > sumaO
-        O = [O sumaI - sumaO]
-        C = [C zeros(m)]
-        n += 1
-    elseif sumaI < sumaO
-        I = [I sumaO - sumaI]
-        C = [C; zeros(n)']
-        m += 1
+    if i != o
+        if i > o
+            novi_redic = [i - o]  
+            I = vcat(I, novi_redic)  
+            novi_red = zeros(1, size(C, 2))  
+            C = vcat(C, novi_red)  
+            m = m+1
+        else
+            nova_kolonica = [o-i;]
+            O = hcat(O, nova_kolonica)
+            nova_kolona = zeros(size(C, 1), 1)  
+            C = hcat(C, nova_kolona)  
+            n = n+1
+        end
     end
-
     A = zeros(m, n)
+    T = 0
     i, j = 1, 1
 
-    # algoritam za inicijalno rješenje
     while i <= m && j <= n
-        if I[i] > O[j]
-            A[i, j] = O[j]
-            I[i] -= O[j]
-            O[j] = 0
-            j += 1
+        if i == m && j == n
+            A[i, j] = I[i]
+            println(C)
+            break
         elseif I[i] < O[j]
             A[i, j] = I[i]
             O[j] -= I[i]
             I[i] = 0
-            i += 1
+            i += 1  # Prelaz u naredni red
+        elseif I[i] > O[j]
+            A[i, j] = O[j]
+            I[i] -= O[j]
+            O[j] = 0
+            j += 1  # Prelaz u narednu kolonu
         else
             A[i, j] = I[i]
-            O[j] = 0
             I[i] = 0
-            i += 1
-            j += 1
+            O[j] = 0
+            j += 1  # Prelaz u narednu kolonu
+            A[i, j] = 0  
+            i += 1  # Prelaz u naredni red
         end
     end
 
+    # Ukupni troškovi
     T = sum(A .* C)
-    return T, A
+    return A, T    
 end
 
-# primjer 1 - 5.1 iz knjige
-I = [100, 120, 140]
-O = [90, 125, 80, 65]
-C = [8 9 4 6; 6 9 5 3; 5 6 7 4]
-T, A = nadji_pocetno_SZU(C, I, O)
+#Testni slučaj iz predavanja 5, strana 6
+C = [
+    8 9 4 6;
+    6 9 5 3;
+    5 6 7 4
+]
+I = [100; 120; 140]
+O = [90 125 80 65]
+A, T = nadji_pocetno_SZU(C, I, O)
+println(A)
+println(T)
 
-println("vrijednost ciljne funkcije T = ", T)
-println("početna matrica rješenja A = ", A)
+#Testni slučaj iz predavanja 6, strana 6
+C = [
+    3 2 5 4;
+    6 4 7 8;
+    1 6 3 7
+]
+I = [1; 1; 1]
+O = [1 1 1 1]
+A, T = nadji_pocetno_SZU(C, I, O)
+println(A)
+println(T)
 
-# primjer 2 - 1 iz zsra
-I = [20, 30, 20, 10]
-O = [40, 10, 30]
-C = [10 12 0; 8 4 3; 6 9 4; 7 8 5]
-T, A = nadji_pocetno_SZU(C, I, O)
+#Testni slučaj sa laboratorijske vjezbe jamboard 1.1 SZU
+C = [
+    2 3 1;
+    5 4 8;
+    5 6 8
+]
+I = [20; 15; 40]
+O = [20 30 25]
+A, T = nadji_pocetno_SZU(C, I, O)
+println(A)
+println(T)
 
-println("vrijednost ciljne funkcije T = ", T)
-println("početna matrica rješenja A = ", A)
 
-# primjer 3 - 1 iz tutorijala 3b
-I = [90, 50, 80]
-O = [30, 50, 40, 70, 30]
-C = [8 18 16 9 10; 10 12 10 3 15; 12 15 7 16 4]
-T, A = nadji_pocetno_SZU(C, I, O)
-
-println("vrijednost ciljne funkcije T = ", T)
-println("početna matrica rješenja A = ", A)
--- zadace/zadaca6.jl
+++ zadace/zadaca6.jl
@@ -3,41 +3,23 @@ m = Model(GLPK.Optimizer)
 
 @variable(m, x1>=0)
 @variable(m, x2>=0)
+@variable(m, x3>=0)
 
-@objective(m, Max, 3x1+2x2)
 
-@constraint(m, constraint1, x1+2x2<=85)
-@constraint(m, constraint2, 2x1+x2 <= 50)
+@objective(m, Max, 3x1+2x2+5x3)
+
+@constraint(m, constraint1, x1+2x2 +x3<=450)
+@constraint(m, constraint2, 3x1+2x3 <= 460)
+@constraint(m, constraint3, x1+4x2 <= 400)
+
 
 optimize!(m)
 
 value(x1)
 value(x2)
+value(x3)
 
 objective_value(m)
 value(constraint1)
 value(constraint2)
-
-
-
-
-m = Model(GLPK.Optimizer)
-
-@variable(m, x1>=0)
-@variable(m, x2>=0)
-
-@objective(m, Max, 3x1+2x2)
-
-@constraint(m, constraint1, x1+2x2<=117)
-@constraint(m, constraint2, 2x1+x2 <= 50)
-
-optimize!(m)
-
-value(x1)
-value(x2)
-
-objective_value(m)
-value(constraint1)
-value(constraint2)
-
 
